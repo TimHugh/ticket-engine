28560c709f9ca8b72d1fa5a3fc03d3b5bddcf430
Tim Heuett <tim.heuett@gmail.com>
2018-02-22 16:49:58 -0800

    remove (no longer used) mock repos

--- common/location_repository_test.go
+++ common/location_repository_test.go
@@ -3,15 +3,3 @@ package common
 import (
 	"testing"
 )
-
-type MockLocationRepository struct {
-	Location *Location
-}
-
-func (m *MockLocationRepository) Find(locationID string) *Location {
-	return m.Location
-}
-
-func (m *MockLocationRepository) Store(location Location) {
-	m.Location = &location
-}
--- common/order_repository_test.go
+++ common/order_repository_test.go
@@ -3,15 +3,3 @@ package common
 import (
 	"testing"
 )
-
-type MockOrderRepository struct {
-	Order Order
-}
-
-func (m *MockOrderRepository) Store(order Order) {
-	m.Order = order
-}
-
-func (m *MockOrderRepository) Find(id string) *Order {
-	return &m.Order
-}
