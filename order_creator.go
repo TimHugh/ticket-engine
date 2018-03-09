cfb3661a1846949a7c68082830e5e110df2752f9
Tim Heuett <tim.heuett@gmail.com>
2018-03-08 20:29:32 -0800

    fix order creator error handling

--- order_creator.go
+++ order_creator.go
@@ -16,10 +16,7 @@ type orderRepository interface {
 }
 
 func (o OrderCreator) Create(orderID string, LocationID string) error {
-	existingOrder, err := o.orderRepository.Find(orderID)
-	if err != nil {
-		return fmt.Errorf("Error finding existing order: %s", err)
-	}
+	existingOrder, _ := o.orderRepository.Find(orderID)
 	if existingOrder != nil {
 		return fmt.Errorf("Couldn't create duplicate order %s.", orderID)
 	}
