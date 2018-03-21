c8882d177b2ff836bdfe447d650ea835769fb978
Tim Heuett <tim.heuett@gmail.com>
2018-03-21 09:08:05 -0700

    WIP - still improving tests

--- mongo/order_repository_test.go
+++ mongo/order_repository_test.go
@@ -7,20 +7,15 @@ import (
 )
 
 func TestFind(t *testing.T) {
-	var s MockSession
-
-	var o OrderRepository
-	o.Session = s
 }
 
 func TestCreateNew(t *testing.T) {
-	var s MockSession
+	s := &MockSession{}
+	o := &OrderRepository{s}
+
 	s.InsertFn = func(docs ...interface{}) error {
 		return nil
 	}
-
-	var o OrderRepository
-	o.Session = s
 
 	order := root.Order{
 		ID:         "id",
@@ -30,6 +25,7 @@ func TestCreateNew(t *testing.T) {
 	if err := o.Create(order); err != nil {
 		t.Errorf("expected nil error but got %s", err)
 	}
-
-	// TODO: finish this
+	// if s.InsertInvoked != true {
+	// 	t.Errorf("expected insert method to be invoked")
+	// }
 }
